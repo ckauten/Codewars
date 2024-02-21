8c1db6e8983ff538891cd557aa53445ee831290e
Cameron Kauten <cameron.kauten@gmail.com>
2024-02-21 12:33:27 -0800

    added function to return min and max of array

--- 7kyu/minAndMaxOfArray.js
+++ 7kyu/minAndMaxOfArray.js
@@ -1,5 +1,5 @@
 // Task
-// Write a function that returns both the minimum and maximum number of the given list/array.
+// Write a function that returns both the minimum and maximum number of the given list/array
 
 // Examples (Input --> Output)
 // [1,2,3,4,5] --> [1,5]
