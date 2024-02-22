1635218f1157abbca33386d132d2b406ae24ee73
Cameron Kauten <cameron.kauten@gmail.com>
2024-02-21 17:15:46 -0800

    changes letter with numeric position in alphabet

--- 6kyu/alphabetLettering.js
+++ 6kyu/alphabetLettering.js
@@ -1,1 +1,20 @@
-// hello to my baby
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+function alphabetPosition(text) {
+    text = text.toLowerCase()
+    text = text.split('')
+    let numerics = []
+    let junk = []
+    for (letter of text){
+      if(/[a-z]/.test(letter)){
+        numerics.push(letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1)
+      }else{
+        junk.push(letter)
+      }
+    }
+    return numerics.join(' ')
+  }
