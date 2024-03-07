a687641fba41636caafa0237bc028ea0fea82b43
Cameron Kauten <cameron.kauten@gmail.com>
2024-03-06 17:36:54 -0800

    function to fix a broken array

---
+++ 8kyu/fixingDaPipe.js
@@ -0,0 +1,15 @@
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,
+
+function pipeFix(numbers) {
+  let arr = [];
+  let base = numbers[0];
+  let cap = numbers[numbers.length - 1];
+  for (let i = base; i <= cap; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
