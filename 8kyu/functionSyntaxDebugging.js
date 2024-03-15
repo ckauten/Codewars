c2c78e392bf7a61e2e40a18fd4e3e5243502d46a
Cameron Kauten <cameron.kauten@gmail.com>
2024-03-15 16:04:16 -0700

    function that corrects a syntax error

---
+++ 8kyu/functionSyntaxDebugging.js
@@ -0,0 +1,11 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+//Start
+//      function main [verb, noun]
+//      return verb + noun
+//      }
+
+//Finish
+function main(verb, noun) {
+  return verb + noun;
+}
